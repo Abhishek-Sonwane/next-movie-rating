773eff2861dfb887e050857acbda9bc192fb8f2d
Abhishek Sonwane <abhisheksonwane629@gmail.com>
2025-02-25 12:19:48 +0530

    Add MongoDb to connect clerk and the database

---
+++ app/src/lib/mongodb/mongoose.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+export const connect = async () => {
+    mongoose.set('strictQuery', true);
+    if(initialized){
+        console.log("MongoDB already Connected");
+        return;
+    }
+    try{
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'next-estate',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        initialized= true;
+        console.log("MongoDB Connected");
+    }catch(error){
+        console.log("MongoDB Connection Error", error);
+        
+    }
+}
